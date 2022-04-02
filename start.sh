6bccfddf1333fb47f105e5fe17b2686e4cf0d968
kinofgl <kinofgl@gmail.com>
2022-04-02 15:45:49 +0800

    修改文件路径

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ day=`date +%d `
 hour=`date +%H`
 now=$year-$month-$day-$hour
 
-
+git diff
 git config --global user.email "kinofgl@gmail.com"
 git config --global user.name "kinofgl"
-
 git add .
 git commit -m "$now"
+git push -u origin master

dd5fefaf57371fe7ba83339d23affc086a72d9ac
kinofgl <kinofgl@gmail.com>
2022-04-02 16:47:08 +0800

    在start.sh获取昨天的日期

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 python3 ./bingUHD.py
 
-time=$(date "+%Y.%m.%d %H%M%S")
+time=$(date "+%Y.%m.%d %H:%M:%S")
 
-month=$(date "+%Y-%m")
+month=$(date -d yesterday "+%Y-%m")
 
 git diff
 git config --global user.email "kinofgl@gmail.com"
 git config --global user.name "kinofgl"
 
 git add BingUHD/"$month"/*.*
-git commit -m "updated: $time"
+git commit -m "updated: $time UTC"
 git push -u origin master

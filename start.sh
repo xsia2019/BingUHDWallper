6af44c11a3d42226e338697f65f09683946db2c4
kinofgl <kinofgl@gmail.com>
2022-04-02 16:28:12 +0800

    调整目录

--- start.sh
+++ start.sh
@@ -1,18 +1,13 @@
 python3 ./bingUHD.py
 
-year=`date +%Y `
-month=`date +%m `
-day=`date +%d `
-hour=`date +%H`
-minute=`date +%M`
-second=`date +%S`
-now=$year.$month.$day.$hour.$minute.$second
-this_month=$year-$month
+time=$(date "+%Y.%m.%d %H%M%S")
+
+month=$(date "+%Y-%m")
 
 git diff
 git config --global user.email "kinofgl@gmail.com"
 git config --global user.name "kinofgl"
 
-git add BingUHD/"$this_month"/*.*
-git commit -m "updated: $now"
+git add BingUHD/"$month"/*.*
+git commit -m "updated: $time"
 git push -u origin master
